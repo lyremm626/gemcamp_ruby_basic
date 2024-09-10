cd77ff9f5fe004f9e8c0bdd8a722e61eb7c6e826
lyremm626 <lyremm@yahoo.com>
2024-09-10 14:33:36 +0800

    answer_22: result of dividing 50

---
+++ printing_variables_operators/activity_22.rb
@@ -0,0 +1,8 @@
+a = 50
+b = 4
+
+result_of_division = a / b
+remainder = 50 % b
+
+puts "Results of dividing 50 by #{b}: #{result_of_division}."
+puts "Remainder when 50 is divided by #{b}: #{remainder}."

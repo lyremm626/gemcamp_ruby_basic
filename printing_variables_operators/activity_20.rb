2f8f47451504a7cdc163232b257b5aa5d1b75593
lyremm626 <lyremm@yahoo.com>
2024-09-10 13:59:16 +0800

    answer_20: user age after 5 yrs.

---
+++ printing_variables_operators/activity_20.rb
@@ -0,0 +1,8 @@
+puts "Birth year: "
+given_number = gets.chomp.to_i
+current_year = 2024
+
+current_age = (current_year - given_number) + 5
+
+puts "Current age after 5 years: #{current_age}"
+
---
+++ printing_variables_operators/activity_21.rb
@@ -0,0 +1,7 @@
+puts "enter a number: "
+given_number = gets.chomp.to_i
+cm_to_m = given_number / 100.0
+
+puts "Height in meters #{+cm_to_m}"
+
+

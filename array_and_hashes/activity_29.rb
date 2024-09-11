3c9601c99c88da49557ae3d35241062227c028a2
lyremm626 <lyremm@yahoo.com>
2024-09-11 14:53:08 +0800

    print person name 2

--- array_and_hashes/activity_29.rb
+++ array_and_hashes/activity_29.rb
@@ -1,4 +1,6 @@
-person1 = {name: "Alice", age: 30}
-person2 = {name: "Bob", age: 25}
+users = {
+  person1: {name: "Alice", age: 30},
+  person2: {name: "Bob", age: 25}
+}
 
-p person2[:name]
+p users[:person2][:name]

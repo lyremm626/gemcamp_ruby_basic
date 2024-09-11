4617e0858efe1039d1b311f9bb784542cff45297
lyremm626 <lyremm@yahoo.com>
2024-09-11 15:23:35 +0800

    10th student

---
+++ array_and_hashes/activity_37.rb
@@ -0,0 +1,13 @@
+students = [
+  {name: "John", age: 18, grade: 89},
+  {name: "Alex", age: 17, grade: 89},
+  {name: "Sarah", age: 16, grade: 89},
+  {name: "Liz", age: 15, grade: 89},
+  {name: "Jay", age: 20, grade: 89},
+  {name: "Joe", age: 21, grade: 89},
+  {name: "Kala", age: 22, grade: 89},
+  {name: "Jed", age: 23, grade: 89},
+  {name: "Ned", age: 24, grade: 89},
+  {name: "Luke", age: 25, grade: 89}
+]
+p students[9][:name]

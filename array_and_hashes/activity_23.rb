99d3c59916f2fd9e7b40153734f0f77e735e7343
lyremm626 <lyremm@yahoo.com>
2024-09-11 14:23:26 +0800

    favorite book, second author

---
+++ array_and_hashes/activity_23.rb
@@ -0,0 +1,5 @@
+first_book = {title: "Harry Potter", author: "J.K Rowling"}
+second_book = {title: "Percy Jackson", author: "Rick Riordan"}
+third_book = {title: "Divergent", author: "Veronica Roth"}
+
+p second_book[:author]
